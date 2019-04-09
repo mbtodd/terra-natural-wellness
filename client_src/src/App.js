b09caf1792ca165d6712dfdaa3dddbb11ecb694b
Michael Todd <michaelbtodd@yahoo.com>
2019-04-08 21:32:19 -0400

    Navbar component added

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+// import { Link } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<div>Hello World</div>
+				{/* <header>
+					<i className="fas fa-bars" />
+					<br />
+					<i className="fab fa-facebook-square" />
+					<i className="fab fa-twitter-square" />
+				</header>
+				<div>Yoga</div> */}
+
+				<Button variant="contained" color="primary">
+					Hello World
+				</Button>
 			</div>
 		);
 	}
